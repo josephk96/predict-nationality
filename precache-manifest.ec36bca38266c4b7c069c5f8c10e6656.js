87037911653edb2528158a0e7d50e56cd987b544
Soo Hwan Kim <soohwan.kim@slexn.com>
2019-10-01 17:29:41 +0900

    Updates

---
+++ precache-manifest.ec36bca38266c4b7c069c5f8c10e6656.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de864ebb1addf57081ec82045642b4ad",
+    "url": "/predict-nationality/index.html"
+  },
+  {
+    "revision": "7808dec5532b9cb2f243",
+    "url": "/predict-nationality/static/js/2.5de0a009.chunk.js"
+  },
+  {
+    "revision": "390c2163d34c3768a5d6",
+    "url": "/predict-nationality/static/js/main.88c25127.chunk.js"
+  },
+  {
+    "revision": "24ef5cbf512eee6465a4",
+    "url": "/predict-nationality/static/js/runtime~main.9b11651b.js"
+  }
+]);
